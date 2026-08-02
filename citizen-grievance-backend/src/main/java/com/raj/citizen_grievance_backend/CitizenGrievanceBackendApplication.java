11598707fb1db226c35923b1e8874d1bfc8bd5f6
Raj Ranjan <raj.ranjan9178@gmail.com>
2026-08-02 16:21:26 +0530

    feat(frontend): setup project foundation and routing

---
+++ citizen-grievance-backend/src/main/java/com/raj/citizen_grievance_backend/CitizenGrievanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.raj.citizen_grievance_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenGrievanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenGrievanceBackendApplication.class, args);
+	}
+
+}
---
+++ citizen-grievance-backend/src/test/java/com/raj/citizen_grievance_backend/CitizenGrievanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.citizen_grievance_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenGrievanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
